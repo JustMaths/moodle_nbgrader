d0a938cd6879edb7ef57a750622b4683283bb212
Justin McInroy <j.f.mcinroy@gmail.com>
2024-04-17 18:31:54 +0100

    Updated to work with Moodle at Chester
    Updated collect_files.py and update_gradesheet.py to work with Moodle at Chester.
    Tidied up files a little too.

--- collect_files.py
+++ collect_files.py
@@ -1,31 +1,51 @@
+######################
+#
+# NB MUST Copy this into the course directory and run from there
+# Code to ingest downloaded student submissions of Jupyter notebooks nbgrader python ipynb homeworks
+# and put these in the correct form for nbgrader to process
+#
+######################
+
 import os
 import re
 import nbgrader, csv, codecs, sys, os, shutil
 from nbgrader.apps import NbGraderAPI
-import zipfile  
+import zipfile
 import shutil
 verbose = False
 
-def moodle_gradesheet(notebook_name, assign_name, csvfile, zip):        
+def moodle_gradesheet(notebook_name, assign_name, csv_file, zip_file):        
 
-    api = NbGraderAPI()    
+    api = NbGraderAPI()
     gradebook = api.gradebook
-    
 
-    archive = zipfile.ZipFile(zip)
+    archive = zipfile.ZipFile(zip_file)
+
+    # IGNORE .csv files
+    ignored_ext = ['csv']
+
     fnames = {}
-
     # read all the filenames, and get the submission
-    # ids for each filename
+    # creates a dictionary of submissions for each student
+    # keys are the moodle participent number ("Identifier")
+    # values are a sequence of files matching this
     for f in archive.filelist:
         fname = f.filename
+        
+        # There should be a better way to code this
+        if True in [ fname.endswith('.'+ext) for ext in ignored_ext ]:
+            continue
+        
         match = re.match("[\*\w\-\'\s\.]+_([0-9]+)_.*", fname)
         if match:
-            fnames[match.groups()[0]] = fname
+            if match.groups()[0] in fnames.keys():
+                fnames[match.groups()[0]].append(fname)
+            else:
+                fnames[match.groups()[0]] = [fname]
         else:
             print("Did not match ", fname)
 
-    with open(csvfile, newline='', encoding='utf-8-sig') as f:
+    with open(csv_file, newline='', encoding='utf-8-sig') as f:
         reader = csv.DictReader(f) 
         assign_matric = {} 
         n_rows = 0
@@ -35,13 +55,13 @@ def moodle_gradesheet(notebook_name, assign_name, csvfile, zip):
             
         for line in reader:        
             
-            ident, fullname,email, status,  grade, max_grade = (line['Identifier'], line['Full name'], line["Email address"], 
+            ident, fullname, email, status, grade, max_grade = (line['Identifier'], line['Full name'], line["Email address"], 
                                                                 line['Status'], line['Grade'], line['Maximum Grade'])
 
             should_be_submission =  "Submitted" in status
 
             # make sure we have this student in our records
-            unique_id = email[0:7]
+            unique_id = email[0:email.index('@')]
             try:
                 result = gradebook.find_student(unique_id)
             except nbgrader.api.MissingEntry:
@@ -50,7 +70,7 @@ def moodle_gradesheet(notebook_name, assign_name, csvfile, zip):
 
                 
             # map assignment numbers to matric numbers
-            matric = email[0:7]
+            matric = email[0:email.index('@')]
             match = re.match('Participant ([0-9]+)', ident)
             if not match:
                 print(f"Could not find identity for participant {ident}")
@@ -67,18 +87,22 @@ def moodle_gradesheet(notebook_name, assign_name, csvfile, zip):
                     os.makedirs(submission_path)
                 except:
                     pass
-                fname = fnames[ident]
-                notebook_file = os.path.basename(fname)
-                notebook_file = notebook_name+".ipynb"
-                if verbose:
-                    print("Extracting {notebook} to {path}".format(notebook=notebook_file, path=submission_path))
-
-                source = archive.open(fname)
-                target = open(os.path.join(submission_path, notebook_file), "wb")
-                with source, target:
-                    shutil.copyfileobj(source, target)
                 
-                successful_files += 1
+                for fname in fnames[ident]:
+                    # if the file ends with ipynb, then rename, otherwise don't
+                    if fname.endswith('.ipynb'):
+                        notebook_file = notebook_name + ".ipynb"
+                    else:
+                        notebook_file = fname
+                    if verbose:
+                        print("Extracting {notebook} to {path}".format(notebook=notebook_file, path=submission_path))
+
+                    source = archive.open(fname)
+                    target = open(os.path.join(submission_path, notebook_file), "wb")
+                    with source, target:
+                        shutil.copyfileobj(source, target)
+                    
+                    successful_files += 1
             else:
                 # submission was in the CSV file, but we don't have a zip file
                 if should_be_submission:
@@ -91,13 +115,13 @@ def moodle_gradesheet(notebook_name, assign_name, csvfile, zip):
                     missing_files +=1
 
         # print out a summary of what was processed
-        print("""{n_files:d} succesfully extracted of {total_zip:d} files in the ZIP archive.
+        print("""{n_files:d} succesfully extracted of {total_zip:d} non-ignored files in the ZIP archive.
 {missing:d} files were not submitted, as expected.
 {problem:d} files were missing, but showed as submitted on Moodle.
-{total:d} records were processed, and {total_csv} rows in the CSV.
+{total:d} records were processed, for {total_csv} rows in the CSV.
 """.format(n_files=successful_files, missing=missing_files, problem=problem_files,
 total=successful_files+missing_files+problem_files,
-total_zip = len(fnames), total_csv=n_rows))
+total_zip = sum([ len(v) for v in fnames.values()]), total_csv=n_rows))
             
 
     
@@ -107,19 +131,41 @@ if len(sys.argv)!=3:
     print("""
         Usage:
         
-        collect_files.py <assignment> <notebook_name>
+        collect_files.py <assignment_id> <notebook_name>
         
-        # must have the following files in imports/
+        # must have exactly two files in imports/ being
 
             <assignment>.csv <assignment>.zip
 
-        The results will be copied into submitted/<matric_id>/<submission_name>/submission.ipynb
+        The results will be copied into submitted/<matric_id>/<assignment_id>/
+        
+        NB You MUST run this script from the course directory
+           The assignment_id must be the same as the folder in nbgrader eg coursework_1.
+           The notebook name must be the same as the ipynb filename is the release/<assignmen_id> folder in nbgrader
+           The ipynb submission will be renamed <notebook_name>.ipynb; all other files will not.
 
     """)
     sys.exit()
 
+assign_id, notebook_name = sys.argv[1], sys.argv[2]
 
-assign_name, notebook_name = sys.argv[1], sys.argv[2]
-gradesheet_csv, archive_fname = os.path.join("imports", assign_name+".csv"), os.path.join("imports", assign_name+".zip")
+import_dir = "imports"
 
-moodle_gradesheet(notebook_name, assign_name, gradesheet_csv, archive_fname)
+if len(os.listdir(import_dir)) != 2:
+    print("There are more than two files in the import directory.  Please only have the submission zip and grading worksheet csv file here.")
+    sys.exit()
+
+zip_file = [s for s in os.listdir(import_dir) if s.endswith('.zip')]
+csv_file = [s for s in os.listdir(import_dir) if s.endswith('.csv')]
+if len(zip_file) != 1:
+    print("Can't find the submission zip in", import_dir)
+    sys.exit()
+
+if len(csv_file) != 1:
+    print("Can't find the grading worksheet csv file in", import_dir)
+    sys.exit()
+
+zip_file = os.path.join(import_dir, zip_file[0])
+csv_file = os.path.join(import_dir, csv_file[0])
+
+moodle_gradesheet(notebook_name, assign_id, csv_file, zip_file)
